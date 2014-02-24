ef7cc0ef3eb16cff4b9829adea7daac6dd1d992b
Ben Daniszewski <diggerdudethatdigs@gmail.com>
2014-02-24 06:17:52 -0500

    done

---
+++ Assets/Trigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+	public GameObject balloon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		balloon.constantForce.force = new Vector3(0,5,0);
+
+	}
+}
